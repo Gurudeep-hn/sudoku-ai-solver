b8e9b8cc9a1ce918bc9b5f5c27bfbbbf316008f4
Gurudeep <86560449+Gurudeep-hn@users.noreply.github.com>
2025-05-29 01:34:30 +0200

    Add src package structure

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,9 @@
+"""
+Sudoku AI Solver Package
 
+A sophisticated AI system for reading and solving Sudoku puzzles from images.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Gurudeep Haleangadi Nagesh"
+__email__ = "gurudeep409@gmail.com"
